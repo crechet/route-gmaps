0974a66c8276a22bb5d53b5e8590cbeb445f34e7
Alexander <aiadamov182@gmail.com>
2018-01-16 15:35:31 +0300

    Add base project structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import Root from './components/root';
+import store from './store';
+
+ReactDOM.render(
+    <Provider store={ store }>
+        <Root />
+    </Provider>,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
---
+++ src/store.js
@@ -0,0 +1,13 @@
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+import rootReducer from './reducers/rootReducer';
+
+const store = createStore(
+    rootReducer,
+    {},
+    applyMiddleware(reduxThunk)
+);
+
+console.log(store.getState());
+
+export default store;
