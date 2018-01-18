d5380d33ac04cdc56284ab7564738c223dd8dcf4
Alexander <aiadamov182@gmail.com>
2018-01-18 10:24:50 +0300

    Google maps initialization

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './map.css';
 
-export default () => {
-    return(
-        <div className="map">
-            <p className="map-caption">Map</p>
-        </div>
-    );
+/**
+ * Wrapper component for external map library. Manage all relationship between react and map library.
+ * */
+
+export default class Map extends Component {
+    // Never rerender.
+    shouldComponentUpdate() {
+        return false;
+    }
+
+    componentDidMount() {
+        window.initMap = this.initMap.bind(this);
+        this.loadMap();
+    }
+
+    loadMap() {
+        const scriptTag = window.document.getElementsByTagName('script')[0];
+        const scriptMap = window.document.createElement('script');
+        scriptMap.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDp39kg5fDCh-WnF2DmhBT4-yYVoVO3HVg&callback=initMap';
+        scriptMap.async = true;
+        scriptMap.defer = true;
+        scriptTag.parentNode.insertBefore(scriptMap, scriptTag);
+    }
+
+    initMap() {
+        console.log('try init map');
+        this.map = new window.google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
+        });
+    }
+
+    render() {
+        return(
+            <div id="map" className="map"></div>
+        );
+    }
 }
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,10 @@
+/* Points related constants */
 export const ADD_POINT = 'ADD_POINT';
+
+/* Map related constants */
+// bad
+export const MAP_API_KEY = 'AIzaSyDp39kg5fDCh-WnF2DmhBT4-yYVoVO3HVg';
+export const MAP_API = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap';
+
+
+// https://geocode-maps.yandex.ru/1.x/?geocode=%D0%9C%D0%BE%D1%81%D0%BA%D0%B2%D0%B0&format=json
